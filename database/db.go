1b43180c49dc3cbb785c1b6eda9f0364cade9a76
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-12-06 01:49:50 +0700

    Update Db Password

--- database/db.go
+++ database/db.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=localhost user=postgres password='Postgres@123' dbname=bioskopdb port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password='FTMdCENFpEThEWHGvTwyvebqYZtOAENI' dbname=bioskopdb port=5432 sslmode=disable"
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
